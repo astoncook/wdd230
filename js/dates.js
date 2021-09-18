bfa3c30d5321a27d6447a27f9a970c99b6aa1bca
Aston Cook <coo19025@byui.edu>
2021-09-18 18:55:14 -0500

    Saturday Chnages

---
+++ js/dates.js
@@ -0,0 +1,5 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear;
+
+alert(document.lastModified);
+let oLastModif = new Date(document.lastModified);
