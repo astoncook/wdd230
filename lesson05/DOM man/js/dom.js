000c85f9ed53034d067e3ed5494635ed4d8e4b1b
astoncook <coo19025@byui.edu>
2021-10-14 14:15:49 -0500

    dom manipluation

---
+++ lesson05/DOM man/js/dom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("input");
+const list = document.querySelector("ul");
+const button = document.querySelector("button");
+const main = document.querySelector("main");
+
+button.addEventListener("click", function() {
+    let inputText = input.value;
+
+    if (inputText === "") {
+    }
+
+    else {
+        const listItem = document.createElement("li")
+        const listButton = document.createElement("button");
+        listItem.textContent = inputText;
+        listButton.textContent = "❌"
+        listItem.appendChild(listButton);
+        list.appendChild(listItem);
+        listButton.addEventListener("click", function() {
+            list.removeChild(listItem);
+        })
+    }
+    input.focus();
+    input.value = "";
+})
