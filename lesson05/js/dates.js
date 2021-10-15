3241e9e80a80a5cc4ce0707743e5a296d4840521
astoncook <coo19025@byui.edu>
2021-10-15 13:35:41 -0500

    lesson 5

---
+++ lesson05/js/dates.js
@@ -0,0 +1,8 @@
+// get a new date (locale machine date time)
+var date = new Date();
+// get the date as a string
+var n = date.toDateString();
+// get the time as a string
+var time = date.toLocaleTimeString();
+
+document.getElementById('date').innerHTML = n + ' ' + time;
