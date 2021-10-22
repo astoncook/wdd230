6c3919433df6eace58cf30a0ef420e40e9464992
astoncook <coo19025@byui.edu>
2021-10-22 15:10:19 -0500

    clean up

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -1,12 +1,9 @@
-let Temp = document.getElementById('temperature').textContent;
-let speedW = document.getElementById('windspeed').textContent;
+let TEMPERATURE = document.getElementById('temperature').textContent;
+let WINDSPEED = document.getElementById('windspeed').textContent;
 
-if(Temp <= 50 && speedW >= 3){
-  let p = Math.pow(speedW, 0.16);
-  let f = 35.74 + 0.6215 * Temp - 35.75 * p + 0.4275 * Temp * p;
-  let result = f.toFixed(0);
+if(TEMPERATURE <= 50 && WINDSPEED >= 3){
+  let X = Math.pow(WINDSPEED, 0.16);
+  let I = 35.74 + 0.6215 * TEMPERATURE - 35.75 * X + 0.4275 * TEMPERATURE * X;
+  let result = I.toFixed(0);
   document.getElementById('windchill').innerHTML = result + "&deg;F";
-}
-else{
-   document.getElementById('windchill').textContent = "N/A";
 }
